4554688c54ba2176fd57b1d3c04646169efa9880
retrofox <retrofox@users.noreply.github.com>
2025-01-13 22:16:34 +0000

    PieChart: iterate over component. Introduce `size`, `thickness` and `padding` property (#40993)
    * replace width and height with size
    This is a Pie chart. Size is more accurate
    
    * introduce padding prop
    
    * changelog
    
    * tweak legendOrientation story control
    
    * change thickness arg story order
    
    * update thickness value in storieps
    
    * computer outer, inner, and thickness props
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12756880239
    
    Upstream-Ref: Automattic/jetpack@d34f9d450b4a62d121849a82b89bfbb63db4d6fc

--- dist/types/components/pie-chart/pie-chart.d.ts
+++ dist/types/components/pie-chart/pie-chart.d.ts
@@ -1,9 +1,22 @@
 import type { BaseChartProps, DataPointPercentage } from '../../types';
-interface PieChartProps extends BaseChartProps<DataPointPercentage[]> {
+type OmitBaseChartProps = Omit<BaseChartProps<DataPointPercentage[]>, 'width' | 'height'>;
+interface PieChartProps extends OmitBaseChartProps {
     /**
      * Inner radius in pixels. If > 0, creates a donut chart. Defaults to 0.
      */
     innerRadius?: number;
+    /**
+     * Size of the chart in pixels
+     */
+    size?: number;
+    /**
+     * Add padding to the chart
+     */
+    padding?: number;
+    /**
+     * Thickness of the pie chart. A value between 0 and 1
+     */
+    thickness?: number;
 }
 declare const _default: (props: Omit<PieChartProps, "width" | "height">) => import("react/jsx-runtime").JSX.Element;
 export default _default;
