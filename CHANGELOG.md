dd45d276b22e443dad5f0a170b95fa769de8088f
retrofox <retrofox@users.noreply.github.com>
2025-01-14 20:19:11 +0000

    PieChart: Introduce padScale and cornerScale properties (#41033)
    * introduce gapScale to PieChart char
    
    * introduce cornerRadius PieChart property
    
    * introduce cornerScale property
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12775453107
    
    Upstream-Ref: Automattic/jetpack@f7659a27bf1757b14fbabf674328f1c9a117ab5e

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Changed
+- Introduce gapScale and cornerScale properties
 - PieChart: iterate a bit over component API
 
 ## [0.3.0] - 2025-01-12
--- dist/types/components/pie-chart/pie-chart.d.ts
+++ dist/types/components/pie-chart/pie-chart.d.ts
@@ -14,9 +14,21 @@ interface PieChartProps extends OmitBaseChartProps {
      */
     padding?: number;
     /**
-     * Thickness of the pie chart. A value between 0 and 1
+     * Thickness of the pie chart.
+     * A value between 0 and 1, where 0 means no thickness
+     * and 1 means the maximum thickness.
      */
     thickness?: number;
+    /**
+     * Scale of the gap between groups in the pie chart
+     * A value between 0 and 1, where 0 means no gap.
+     */
+    gapScale?: number;
+    /**
+     * Scale of the corner radius for the pie chart segments.
+     * A value between 0 and 1, where 0 means no corner radius.
+     */
+    cornerScale?: number;
 }
 declare const _default: (props: Omit<PieChartProps, "width" | "height">) => import("react/jsx-runtime").JSX.Element;
 export default _default;
