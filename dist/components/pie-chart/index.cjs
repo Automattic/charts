5359e1ab7f2736b585534cc6f0b26a9473cbcb77
tbradsha <tbradsha@users.noreply.github.com>
2025-10-09 13:12:48 +0000

    Comment Likes: Prevent PHP warnings when home URL is invalid (#45420)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18376946264
    
    Upstream-Ref: Automattic/jetpack@a678895762653ac2b394570ffddc7bc419980256

--- dist/components/pie-chart/index.cjs
+++ dist/components/pie-chart/index.cjs
@@ -1,10 +1,10 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});
 
 
-var _chunk6MUFPDNRcjs = require('../../chunk-6MUFPDNR.cjs');
+var _chunkXJSZXQONcjs = require('../../chunk-XJSZXQON.cjs');
 require('../../chunk-7OZEQ5HE.cjs');
-require('../../chunk-3Z526IL2.cjs');
 require('../../chunk-6GO5PFYL.cjs');
+require('../../chunk-3Z526IL2.cjs');
 require('../../chunk-FX2PTUFC.cjs');
 require('../../chunk-76XOLO7F.cjs');
 require('../../chunk-TEEQFJFL.cjs');
@@ -12,5 +12,5 @@ require('../../chunk-EMMSS5I5.cjs');
 
 
 
-exports.PieChart = _chunk6MUFPDNRcjs.PieChartResponsive; exports.PieChartUnresponsive = _chunk6MUFPDNRcjs.PieChart;
+exports.PieChart = _chunkXJSZXQONcjs.PieChartResponsive; exports.PieChartUnresponsive = _chunkXJSZXQONcjs.PieChart;
 //# sourceMappingURL=index.cjs.map
--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,15 @@
 import {
+  LineChart,
+  LineChartResponsive
+} from "./chunk-LIYP7LQW.js";
+import {
   PieChart,
   PieChartResponsive
-} from "./chunk-L5TSJMD2.js";
+} from "./chunk-M3JU5JTP.js";
 import {
   PieSemiCircleChart,
   PieSemiCircleChartResponsive
-} from "./chunk-OA57H6MV.js";
+} from "./chunk-LD4SAJTJ.js";
 import "./chunk-NFRB2POF.js";
 import {
   BarListChart,
@@ -16,19 +20,15 @@ import {
   BarChartResponsive
 } from "./chunk-OYPAAX3P.js";
 import {
+  BaseTooltip
+} from "./chunk-W5KOH3TV.js";
+import {
   ConversionFunnelChartWithProvider
 } from "./chunk-CES34TN5.js";
 import {
   LeaderboardChartResponsive
 } from "./chunk-IHUHB3JB.js";
 import "./chunk-D3DZT2EK.js";
-import {
-  LineChart,
-  LineChartResponsive
-} from "./chunk-LIYP7LQW.js";
-import {
-  BaseTooltip
-} from "./chunk-W5KOH3TV.js";
 import "./chunk-TYYW4BG3.js";
 import {
   Legend,
