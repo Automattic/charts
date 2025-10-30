b1e95322e414c4230ac266e0d3bf0ce51039af72
tbradsha <tbradsha@users.noreply.github.com>
2025-10-30 14:02:39 +0000

    Update dependency svgo to v4 (#45678)
    * Remove title and viewbox during optimization
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18942786078
    
    Upstream-Ref: Automattic/jetpack@253c06832014df646d76a148903800262b384482

--- dist/index.js
+++ dist/index.js
@@ -5,16 +5,23 @@ import {
 import {
   PieChart,
   PieChartResponsive
-} from "./chunk-56LSHAQ6.js";
+} from "./chunk-ZSNO2BYX.js";
 import {
   PieSemiCircleChart,
   PieSemiCircleChartResponsive
-} from "./chunk-DZ36HKSB.js";
+} from "./chunk-BWEMZ72V.js";
 import "./chunk-NFRB2POF.js";
 import {
   BarListChart,
   BarListChartResponsive
 } from "./chunk-JI6OGGGF.js";
+import {
+  BarChart,
+  BarChartResponsive
+} from "./chunk-HVWETEEV.js";
+import {
+  BaseTooltip
+} from "./chunk-W5KOH3TV.js";
 import {
   ConversionFunnelChartWithProvider
 } from "./chunk-KEBKTDOQ.js";
@@ -23,13 +30,6 @@ import {
   LeaderboardChartResponsive
 } from "./chunk-4H3J2HCD.js";
 import "./chunk-D3DZT2EK.js";
-import {
-  BarChart,
-  BarChartResponsive
-} from "./chunk-HVWETEEV.js";
-import {
-  BaseTooltip
-} from "./chunk-W5KOH3TV.js";
 import "./chunk-TYYW4BG3.js";
 import {
   Legend,
