0c09a62648fadc2905e16a9e593d573f6c2083a0
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-12-19 10:33:46 +0000

    Social: Remove upload video option from media dropdown (#46356)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20366952303
    
    Upstream-Ref: Automattic/jetpack@767013b5b79aeed3e3a6b6a2023af29bf26c4718

--- dist/charts/bar-chart/index.cjs
+++ dist/charts/bar-chart/index.cjs
@@ -1,15 +1,15 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});
 
 
-var _chunkGWNXOI4Mcjs = require('../../chunk-GWNXOI4M.cjs');
-require('../../chunk-7HROSZRS.cjs');
+var _chunkKSSPQFW4cjs = require('../../chunk-KSSPQFW4.cjs');
 require('../../chunk-MUUSZ7J5.cjs');
 require('../../chunk-NYZFVI2P.cjs');
 require('../../chunk-W3H42XRV.cjs');
 require('../../chunk-DAKYGZG6.cjs');
+require('../../chunk-7HROSZRS.cjs');
 require('../../chunk-EMMSS5I5.cjs');
 
 
 
-exports.BarChart = _chunkGWNXOI4Mcjs.BarChartResponsive; exports.BarChartUnresponsive = _chunkGWNXOI4Mcjs.BarChart;
+exports.BarChart = _chunkKSSPQFW4cjs.BarChartResponsive; exports.BarChartUnresponsive = _chunkKSSPQFW4cjs.BarChart;
 //# sourceMappingURL=index.cjs.map
