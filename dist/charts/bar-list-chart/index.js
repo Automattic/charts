23ebd6df4a5ce3332e5cad2216af1b49a07ce92e
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-12-22 05:27:22 +0000

    Social: Update unified modal footer button labels (#46372)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20422394235
    
    Upstream-Ref: Automattic/jetpack@d79b08096ee1688a14fb21c909d326b8bbc5b341

--- dist/charts/bar-list-chart/index.js
+++ dist/charts/bar-list-chart/index.js
@@ -1,13 +1,13 @@
 import {
   BarListChart,
   BarListChartResponsive
-} from "../../chunk-JQ3274XY.js";
-import "../../chunk-4GIFN3HS.js";
+} from "../../chunk-W4ZYJ74Q.js";
+import "../../chunk-DY7IVYWP.js";
+import "../../chunk-5XI443YP.js";
 import "../../chunk-NONODB3K.js";
 import "../../chunk-VER6S543.js";
 import "../../chunk-O2BJMTIS.js";
 import "../../chunk-P3QEXFTA.js";
-import "../../chunk-5XI443YP.js";
 import "../../chunk-G3PMV62Z.js";
 export {
   BarListChartResponsive as BarListChart,
