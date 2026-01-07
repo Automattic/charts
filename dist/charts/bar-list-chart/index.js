02b67a94e7aa8af73008dae7ac4d27605fc18dca
dzver <dzver@users.noreply.github.com>
2026-01-07 16:07:35 +0000

    Update/feature gating for donations block jetpack change v2 (#46477)
    * Feature gating for the Donations block v2
    
    This is the Jetpack change
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20787244328
    
    Upstream-Ref: Automattic/jetpack@e40731c8e390f834a412b7d1d875967590b607d1

--- dist/charts/bar-list-chart/index.js
+++ dist/charts/bar-list-chart/index.js
@@ -1,11 +1,11 @@
 import {
   BarListChart,
   BarListChartResponsive
-} from "../../chunk-RD3H5QSL.js";
-import "../../chunk-Z5DELZV2.js";
+} from "../../chunk-KORG7ITC.js";
+import "../../chunk-KBORJZKC.js";
+import "../../chunk-ALDWCNLH.js";
 import "../../chunk-5XI443YP.js";
 import "../../chunk-NONODB3K.js";
-import "../../chunk-ALDWCNLH.js";
 import "../../chunk-DYMJWNYM.js";
 import "../../chunk-TE63Y5PX.js";
 import "../../chunk-G3PMV62Z.js";
--- dist/charts/line-chart/index.cjs
+++ dist/charts/line-chart/index.cjs
@@ -1,15 +1,15 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});
 
 
-var _chunkOABHFSI3cjs = require('../../chunk-OABHFSI3.cjs');
+var _chunkLNLHCZ6Fcjs = require('../../chunk-LNLHCZ6F.cjs');
+require('../../chunk-ZPJHWKEK.cjs');
 require('../../chunk-7HROSZRS.cjs');
 require('../../chunk-MUUSZ7J5.cjs');
-require('../../chunk-ZPJHWKEK.cjs');
 require('../../chunk-WKN6C4ZE.cjs');
 require('../../chunk-ZVGEDXDP.cjs');
 require('../../chunk-EMMSS5I5.cjs');
 
 
 
-exports.LineChart = _chunkOABHFSI3cjs.LineChartResponsive; exports.LineChartUnresponsive = _chunkOABHFSI3cjs.LineChart;
+exports.LineChart = _chunkLNLHCZ6Fcjs.LineChartResponsive; exports.LineChartUnresponsive = _chunkLNLHCZ6Fcjs.LineChart;
 //# sourceMappingURL=index.cjs.map
