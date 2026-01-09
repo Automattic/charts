78974d0c60db84626451b2bc59226f11f5634bd1
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-01-09 05:22:18 +0000

    Update dependency react-router to v7.12.0 [SECURITY] (#46512)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20841851071
    
    Upstream-Ref: Automattic/jetpack@1580db1bd968e3f2acc99ec184e1195b2975e16b

--- dist/charts/bar-chart/index.cjs
+++ dist/charts/bar-chart/index.cjs
@@ -1,15 +1,15 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});
 
 
-var _chunkDDV5726Qcjs = require('../../chunk-DDV5726Q.cjs');
-require('../../chunk-ZPJHWKEK.cjs');
-require('../../chunk-7HROSZRS.cjs');
+var _chunkUCQDBOWAcjs = require('../../chunk-UCQDBOWA.cjs');
 require('../../chunk-MUUSZ7J5.cjs');
+require('../../chunk-ZPJHWKEK.cjs');
 require('../../chunk-WKN6C4ZE.cjs');
 require('../../chunk-ZVGEDXDP.cjs');
+require('../../chunk-7HROSZRS.cjs');
 require('../../chunk-EMMSS5I5.cjs');
 
 
 
-exports.BarChart = _chunkDDV5726Qcjs.BarChartResponsive; exports.BarChartUnresponsive = _chunkDDV5726Qcjs.BarChart;
+exports.BarChart = _chunkUCQDBOWAcjs.BarChartResponsive; exports.BarChartUnresponsive = _chunkUCQDBOWAcjs.BarChart;
 //# sourceMappingURL=index.cjs.map
