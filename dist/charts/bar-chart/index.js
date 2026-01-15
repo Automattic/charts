e756bded0324d7367898a9f99936fac2b950d9a6
jeherve <jeherve@users.noreply.github.com>
2026-01-15 16:21:50 +0000

    Labels: update doc and scripts used to create issues (#46619)
    Following #46571 and #46573, we can stop recommending and using labels with the [Type] prefixes in the repo.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21037831032
    
    Upstream-Ref: Automattic/jetpack@ac864ccc498cf5c489c8169a92419dfe230e6fb0

--- dist/charts/bar-chart/index.js
+++ dist/charts/bar-chart/index.js
@@ -1,12 +1,12 @@
 import {
   BarChart,
   BarChartResponsive
-} from "../../chunk-Y7DDJVJY.js";
-import "../../chunk-NONODB3K.js";
+} from "../../chunk-KBORJZKC.js";
 import "../../chunk-ALDWCNLH.js";
+import "../../chunk-5XI443YP.js";
+import "../../chunk-NONODB3K.js";
 import "../../chunk-DYMJWNYM.js";
 import "../../chunk-TE63Y5PX.js";
-import "../../chunk-5XI443YP.js";
 import "../../chunk-G3PMV62Z.js";
 export {
   BarChartResponsive as BarChart,
