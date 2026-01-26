b0a7662c8c0bcf2231bfd183231d61eeb7d31525
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-01-26 07:35:41 +0000

    Changelog and readme.txt edits.
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21349526389
    
    Upstream-Ref: Automattic/jetpack@67a2cc45d7a39288cc5ce6e0b9cbef727a8289a3

--- dist/charts/line-chart/index.js
+++ dist/charts/line-chart/index.js
@@ -1,12 +1,12 @@
 import {
   LineChart,
   LineChartResponsive
-} from "../../chunk-LLGJAVYI.js";
-import "../../chunk-NONODB3K.js";
+} from "../../chunk-Z34VYZGR.js";
 import "../../chunk-ALDWCNLH.js";
+import "../../chunk-5XI443YP.js";
+import "../../chunk-NONODB3K.js";
 import "../../chunk-DYMJWNYM.js";
 import "../../chunk-TE63Y5PX.js";
-import "../../chunk-5XI443YP.js";
 import "../../chunk-G3PMV62Z.js";
 export {
   LineChartResponsive as LineChart,
--- dist/charts/pie-semi-circle-chart/index.cjs
+++ dist/charts/pie-semi-circle-chart/index.cjs
@@ -1,17 +1,17 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});
 
 
-var _chunkRS6OFMONcjs = require('../../chunk-RS6OFMON.cjs');
+var _chunkUWAZGLHGcjs = require('../../chunk-UWAZGLHG.cjs');
 require('../../chunk-OTZT3MC2.cjs');
 require('../../chunk-HIWNB5PK.cjs');
-require('../../chunk-MUUSZ7J5.cjs');
 require('../../chunk-ZPJHWKEK.cjs');
+require('../../chunk-7HROSZRS.cjs');
+require('../../chunk-MUUSZ7J5.cjs');
 require('../../chunk-WKN6C4ZE.cjs');
 require('../../chunk-ZVGEDXDP.cjs');
-require('../../chunk-7HROSZRS.cjs');
 require('../../chunk-EMMSS5I5.cjs');
 
 
 
-exports.PieSemiCircleChart = _chunkRS6OFMONcjs.PieSemiCircleChartResponsive; exports.PieSemiCircleChartUnresponsive = _chunkRS6OFMONcjs.PieSemiCircleChart;
+exports.PieSemiCircleChart = _chunkUWAZGLHGcjs.PieSemiCircleChartResponsive; exports.PieSemiCircleChartUnresponsive = _chunkUWAZGLHGcjs.PieSemiCircleChart;
 //# sourceMappingURL=index.cjs.map
