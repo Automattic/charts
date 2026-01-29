03ef698f95501881827ba1bc9f8cb3f6513bd1a3
CGastrell <CGastrell@users.noreply.github.com>
2026-01-29 14:07:44 +0000

    Forms: Consolidate inspector response-view components and add shared utilities (#46799)
    * Forms: Add shared inspector utils and PreviewFile component
    
    - Add inspector/utils.ts with shared helpers: getDisplayName,
      isFileUploadField, isImageSelectField, isLikelyPhoneNumber,
      and isCollectionFormatField
    - Add inspector/preview-file.tsx for image file preview (used by
      response view body and route)
    
    * Forms: Use shared inspector utils and PreviewFile in response view
    
    - Response view body: import helpers and PreviewFile from shared
      inspector modules; remove local helpers and PreviewFile; type
      previewFile state and add null check for mobile preview
    - Response route: import helpers, PreviewFile, and inspector styles;
      remove local helpers and inline PreviewFile
    
    * Move response-actions, response-navigation, response-view under inspector
    
    * Add changelog entry
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21480696852
    
    Upstream-Ref: Automattic/jetpack@4a9258b723a30c3602ccd98a70e25b596323b953

--- dist/chunk-OL2DDZZB.js
+++ dist/chunk-OL2DDZZB.js
@@ -1,6 +1,6 @@
 import {
   BarChartResponsive
-} from "./chunk-PU2UO4EG.js";
+} from "./chunk-RNDPQ7C3.js";
 import {
   withResponsive
 } from "./chunk-NONODB3K.js";
@@ -193,4 +193,4 @@ export {
   BarListChart,
   BarListChartResponsive
 };
-//# sourceMappingURL=chunk-2NRSBKV2.js.map
+//# sourceMappingURL=chunk-OL2DDZZB.js.map
