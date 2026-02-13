50ec5dd4c2bf88f2421a80fa574f5e6c820f076b
simison <simison@users.noreply.github.com>
2026-02-13 15:57:58 +0000

    Forms: allow more core blocks inside forms (#47121)
    * Allow core/accordion, core/icon, core/details in Forms
    * Allow core/icon block inside file upload dropzone
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21992805813
    
    Upstream-Ref: Automattic/jetpack@871d6095b9e7cf62549c06b1c93f0c4ad73f97db

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,8 +1,5 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});
 
-var _chunkSRXJLAKGcjs = require('./chunk-SRXJLAKG.cjs');
-
-
 
 var _chunkODF5O5PVcjs = require('./chunk-ODF5O5PV.cjs');
 
@@ -24,6 +21,9 @@ var _chunkXVMXWV3Ccjs = require('./chunk-XVMXWV3C.cjs');
 
 
 var _chunkKOF32DBLcjs = require('./chunk-KOF32DBL.cjs');
+
+
+var _chunkSRXJLAKGcjs = require('./chunk-SRXJLAKG.cjs');
 
 
 
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,4 @@
 import {
-  TrendIndicator
-} from "./chunk-MMDLXS6O.js";
-import {
   LeaderboardChart,
   LeaderboardChartResponsive
 } from "./chunk-TNRKEBTA.js";
@@ -24,6 +21,9 @@ import {
   LineChart,
   LineChartResponsive
 } from "./chunk-ZDNCF642.js";
+import {
+  TrendIndicator
+} from "./chunk-MMDLXS6O.js";
 import {
   BarListChart,
   BarListChartResponsive
