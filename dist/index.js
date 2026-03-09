603e1ba0908ac67d796c68a64c00c593c2595c84
sdnunca <sdnunca@users.noreply.github.com>
2026-03-09 08:27:02 +0000

    renovate: Improve PR creation reliability (#47502)
    Set pnpm's `allowUnusedPatches` to avoid erroring out for cases like
    https://github.com/Automattic/jetpack/pull/47472#issuecomment-4007148761.
    Too bad pnpm 11 is removing `ignorePatchFailures`, that'd be useful for
    the PR creation too.
    
    Also, try creating the change entries even if `pnpm install` fails, as
    the change entry creation doesn't depend on pnpm. The later
    `tools/check-intra-monorepo-deps.sh` run will fail, but getting change
    entries created is still an improvement.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22844210192
    
    Upstream-Ref: Automattic/jetpack@e211e42c6932b638fb6dfd09e506a4975200c2eb

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,4 @@
 import {
-  LeaderboardChart,
-  LeaderboardChartResponsive
-} from "./chunk-2NCY7R4G.js";
-import {
   PieChart,
   PieChartResponsive
 } from "./chunk-BCX5THDQ.js";
@@ -12,7 +8,6 @@ import {
   PieSemiCircleChartResponsive
 } from "./chunk-V36ERY7Y.js";
 import "./chunk-KXRWNFQJ.js";
-import "./chunk-6CCZL2JJ.js";
 import {
   Sparkline,
   SparklineUnresponsive
@@ -21,6 +16,11 @@ import {
   LineChart,
   LineChartResponsive
 } from "./chunk-WLODYNLB.js";
+import {
+  LeaderboardChart,
+  LeaderboardChartResponsive
+} from "./chunk-2NCY7R4G.js";
+import "./chunk-6CCZL2JJ.js";
 import {
   TrendIndicator
 } from "./chunk-XWYZIFZW.js";
