52626f858cde36caf0fc32d47d49b767cba34bae
robertsreberski <robertsreberski@users.noreply.github.com>
2026-06-22 08:44:18 +0000

    feat(premium-analytics): integrate cookie-consent (#49736)
    * feat(premium-analytics): integrate cookie-consent as a dev consumer
    
    * chore(premium-analytics): update composer.lock for cookie-consent mirror config
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27939604463
    
    Upstream-Ref: Automattic/jetpack@42a877840cb480bab5e7d13ae295e7fa394d25f6

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -837,7 +837,7 @@ declare const BarListChartResponsive: ({
   width,
   height,
   ...chartProps
-}: Omit<BarListChartProps, "size" | "width" | "height"> & {
+}: Omit<BarListChartProps, "width" | "height" | "size"> & {
   width?: number;
   height?: number;
   size?: number;
@@ -981,7 +981,7 @@ declare const GeoChartResponsive: ({
   width,
   height,
   ...chartProps
-}: Omit<GeoChartProps, "size" | "width" | "height"> & {
+}: Omit<GeoChartProps, "width" | "height" | "size"> & {
   width?: number;
   height?: number;
   size?: number;
@@ -1057,7 +1057,7 @@ declare const LeaderboardChartResponsive: (({
   width,
   height,
   ...chartProps
-}: Omit<LeaderboardChartProps, "size" | "width" | "height"> & {
+}: Omit<LeaderboardChartProps, "width" | "height" | "size"> & {
   width?: number;
   height?: number;
   size?: number;
@@ -1564,7 +1564,7 @@ declare const Sparkline: ({
   width,
   height,
   ...chartProps
-}: Omit<SparklineProps, "size" | "width" | "height"> & {
+}: Omit<SparklineProps, "width" | "height" | "size"> & {
   width?: number;
   height?: number;
   size?: number;
